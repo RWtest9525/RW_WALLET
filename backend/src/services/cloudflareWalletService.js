5fb1c49b80fe901c412e6e0d4cc4dd1055097d04
Yash Vishal <yashvishal95251824@gmail.com>
2026-07-27 15:30:43 +0530

    Fix: resolve syntax error in embedded service account object keys preventing Render node startup

--- backend/src/services/cloudflareWalletService.js
+++ backend/src/services/cloudflareWalletService.js
@@ -54,9 +54,9 @@ function initFirebaseAdmin() {
     client_id: "111195157656269754044",
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40review-world-1312e.iam.gserviceaccount.com",
-    universe_domain": "googleapis.com"
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40review-world-1312e.iam.gserviceaccount.com",
+    universe_domain: "googleapis.com"
   };
 
   return admin.initializeApp({
